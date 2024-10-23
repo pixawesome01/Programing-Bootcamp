beb7c5e661aa87955019ac7c0022fb0559de80af
ClearSkies <161695625+pixawesome01@users.noreply.github.com>
2024-10-23 11:45:22 +1100

    modified:   Task1.py

--- Task1.py
+++ Task1.py
@@ -12,9 +12,9 @@ def convert_list_to_integer(lst):
     """
     sum = 0
     for index, number in enumerate(lst):
-        place_value = (number * (10 ** (len(lst) - index - 1))) 
-        sum = sum + place_value  #first determine the number of zero's: (10 ** (len(lst) - index - 1), the multiply element value and add to sum
-    return sum # For example, lst = [1,2,3] will be sum = 100 + 20 + 3
+        place_value = (number * (10 ** (len(lst) - index - 1))) #detemine the place value
+        sum = sum + place_value  # For example, lst = [1,2,3] will be sum = 100 + 20 + 3
+    return sum 
 
 print(convert_list_to_integer([8,3,5,1])) #prints 8351
 print(convert_list_to_integer([1, 22, 333, 4444])) #prints 1223334444
